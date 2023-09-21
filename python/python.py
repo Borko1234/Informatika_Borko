fa237cd29ee134cdbc7f91fe905a63bb11af23fa
Borko <107365623+Borko1234@users.noreply.github.com>
2023-09-21 10:29:29 +0300

    Add files via upload

---
+++ python/python.py
@@ -0,0 +1,13 @@
+jivotni=['kon','jaba','krokodil','maimuna']
+x,y,z,a=jivotni
+print(x,y,z,a)
+b = input ('Koe jivotno obichash nai mnogo ot tezi trite')
+ime = input ('kak se kazvash ')
+g=int(input ('Na kolko si'))
+print ('Hello',ime)
+print ('You are',g,'years old')
+
+
+
+
+
