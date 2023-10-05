5d555213a877c58bacd969035e6638cf88c70985
Borko <107365623+Borko1234@users.noreply.github.com>
2023-10-05 16:15:37 +0300

    Update funkcii.py

--- python/python.py
+++ python/python.py
@@ -1,12 +1,9 @@
-jivotni=['kon','jaba','krokodil','maimuna']
-x,y,z,a=jivotni
-print(x,y,z,a)
-b = input ('Koe jivotno obichash nai mnogo ot tezi trite')
-ime = input ('kak se kazvash ')
-g=int(input ('Na kolko si'))
-print ('Hello',ime)
-print ('You are',g,'years old')
-
+#funkcii
+''' random '''
+'''
+import random
+print(random.randrange(1,10))
+'''
 
 
 
