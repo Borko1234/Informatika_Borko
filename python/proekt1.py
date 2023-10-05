a8637255db53ca9875bdd977a19c522c5da2f4c2
Borko <107365623+Borko1234@users.noreply.github.com>
2023-10-05 16:14:19 +0300

    Update proekt1.py

--- python/proekt1.py
+++ python/proekt1.py
@@ -11,28 +11,6 @@ P=2*math.pi*r
 print ('liceto e',lice,'P=',P)"""
 
 
-#kordinati i pravougulnik
-#bonus
-#ipmoprt ime na bibliotekata
-x1=float(input())
-y1=float(input())
-x2=float(input())
-y2=float(input())
-if(y1<0):
-    a = y1 + (y2)*-1
-elif(y2<0):
-    a = y2 + (y1) * -1
-elif(y2<0&y1<0):
-    a = -1*(y1 + y2)
-if(x1<0):
-    b = x1 + (x2)*-1
-elif(yx<0):
-    b = x2 + (x1) * -1
-elif(x2<0&x1<0):
-    b = -1*(x1 + x2)
-S=a*b
-print (s)
-
 
 
 
