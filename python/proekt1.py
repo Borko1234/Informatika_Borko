96252ef0feb84fbced28335c31b717dbad60c275
Borko <107365623+Borko1234@users.noreply.github.com>
2023-10-05 16:27:46 +0300

    Update zadachki.py

--- python/proekt1.py
+++ python/proekt1.py
@@ -9,6 +9,14 @@ r=float(input('Vuvedete r '))
 lice=math.pi*r*r
 P=2*math.pi*r
 print ('liceto e',lice,'P=',P)"""
+import random
+x=random.randrange(1,50)
+print('is it fuchable')
+print(x)
+if(x<12):
+    print ('if her age is on the clock she is ready for the cock')
+else:
+    print('expired')
 
 
 
