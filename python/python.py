d5437e47fc8bcbcf1701b9eea1dfb9cfd409ee20
Borko <107365623+Borko1234@users.noreply.github.com>
2023-10-11 15:41:44 +0300

    Update python.py

--- python/python.py
+++ python/python.py
@@ -4,6 +4,19 @@
 import random
 print(random.randrange(1,10))
 '''
+'''zakruglq po golqmo'''
+'''
+import math
+math.ceil(5.45)
+6
+'''
+'''zakruglq po malko'''
+'''
+import math
+math.floor(5.45)
+5
+'''
+
 
 
 
