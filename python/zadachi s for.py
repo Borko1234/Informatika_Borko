0ae021e0f926c665bf37efdd132beb60378bf326
Borko <107365623+Borko1234@users.noreply.github.com>
2023-11-02 10:15:48 +0200

    Update zadachi s for.py

--- python/zadachi s for.py
+++ python/zadachi s for.py
@@ -1,8 +1,10 @@
+#namerete sumata na chetnite
 sum=0
 for n in range(111,132):
     if n%2==0:
         sum = sum + n
 print(sum)
+#sumata na nechetnite
 a=0
 for n in range(131,156):
     if n%2!=0:
