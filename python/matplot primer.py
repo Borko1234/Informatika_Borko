e46e2569cfad512a2a188c4d2446ef2fa27b7789
Borko <107365623+Borko1234@users.noreply.github.com>
2024-03-14 09:44:56 +0200

    Update matplot primer.py

--- python/matplot primer.py
+++ python/matplot primer.py
@@ -19,3 +19,25 @@ plt.show()
 #Two  lines to make our compiler able to draw:
 plt.savefig(sys.stdout.buffer)
 sys.stdout.flush()
+#primer2
+
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(['Angliiski','Matematika','BEL','IT','Muzika','Geografiq','Chas na klasa','Istoriq','Biologiq','Himiq','Fizika','Informatika','Fizichesko','Filosofiq','Nemski'])
+y = np.array([2,6,3,1,1,3,1,3,2,2,2,3,2,2,2])
+plt.plot(x,y, 'o-r',ms=10,mec='b',mfc='b',linewidth = '2')
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
+plt.title("Broi na uchebni chasove prez sedmicata",fontdict = font1)
+plt.ylabel("Broi chasove",fontdict = font2)
+plt.xlabel("Predmet",fontdict = font2)
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
