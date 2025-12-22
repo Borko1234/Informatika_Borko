1061047be53a5c8dc4c335a98190b477b3ce6517
Borko <107365623+Borko1234@users.noreply.github.com>
2025-12-22 12:13:08 +0200

    Update zaqvki.sql

--- SQL/zaqvki.sql
+++ SQL/zaqvki.sql
@@ -1,42 +1,58 @@
 USE MobileOperatorDemo;
-#1
+GO
+
+-- 1
 SELECT *
 FROM Employees
 WHERE Salary > 3000;
-#2
+GO
+
+-- 2
 SELECT FirstName, LastName, JobTitle
 FROM Employees
 WHERE JobTitle = 'Sales Specialist'
   AND TownID = 2;
-#3
+GO
+
+-- 3
 SELECT *
 FROM Customers
 WHERE TownID IN (1, 2);
-#4
+GO
+
+-- 4
 SELECT *
 FROM Customers
 WHERE SubscriptionPlanID IN (1, 3, 5);
-#5
+GO
+
+-- 5
 SELECT *
 FROM Employees
 WHERE Salary BETWEEN 2000 AND 4000;
-#6
+GO
+
+-- 6
 SELECT *
 FROM Customers
 WHERE FirstName LIKE 'M%';
-#7
+GO
+
+-- 7
 SELECT *
 FROM Employees
 ORDER BY Salary DESC;
-#8
+GO
+
+-- 8
 SELECT *
 FROM Employees
 WHERE HireDate > '2015-01-01'
 ORDER BY HireDate ASC;
-#9
+GO
+
+-- 9
 UPDATE Employees
 SET Salary = Salary * 1.10
 WHERE JobTitle = 'Technician';
-
-
-
+GO
